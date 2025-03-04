71665cb767b35cc8d5191898047c0262da46a23b
Jalson1982 <jasmin.fajkic@gmail.com>
2025-03-04 19:58:46 +0100

    Remove node_modules and dist from tracking

--- dist/controllers/leaderboard/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getLeaderboard = void 0;
-const getLeaderboard = async (req, res) => {
-    const { limit = 10 } = req.query;
-};
-exports.getLeaderboard = getLeaderboard;
